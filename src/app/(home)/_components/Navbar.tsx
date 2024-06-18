9da61c29a53933b122bc0ad19565a8bbd91f4ffd
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-06-18 06:37:30 +0300

    style: :art: Make the navbar hide and reappear only when scroll up

--- src/app/(home)/_components/Navbar.tsx
+++ src/app/(home)/_components/Navbar.tsx
@@ -1,10 +1,36 @@
+"use client"
+import { useEffect, useRef } from "react"
 import { Logo } from "./Logo"
 import MobileNavigation from "./mobile-navigation"
 import NavigationLinks from "./navigation-links"
+import "./navbar.css"
 
 const Navbar = () => {
+	let navref = useRef<HTMLDivElement>(null)
+	useEffect(() => {
+		if (!navref.current) return
+		// JavaScript to handle navbar visibility on scroll
+		let lastScrollY = window.scrollY
+		const navbar = navref.current
+
+		window.addEventListener("scroll", () => {
+			if (window.scrollY > lastScrollY) {
+				// Scrolling down
+				navbar.classList.remove("visible-nav")
+				navbar.classList.add("hidden-nav")
+			} else {
+				// Scrolling up
+				navbar.classList.remove("hidden-nav")
+				navbar.classList.add("visible-nav")
+			}
+			lastScrollY = window.scrollY
+		})
+	}, [])
 	return (
-		<div className="fixed  inset-x-0 top-0  z-30 flex  w-full  justify-center border-b bg-background/70 p-4 shadow-lg backdrop-blur-sm">
+		<div
+			ref={navref}
+			className="fixed  inset-x-0 top-0  z-30 flex  w-full  justify-center border-b bg-background/70 p-4 shadow-lg backdrop-blur-sm"
+		>
 			<div className="w-full max-w-7xl">
 				<div className="flex h-14 w-full items-center  justify-between px-6 max-sm:px-2">
 					<Logo />
