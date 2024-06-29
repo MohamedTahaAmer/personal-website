b4ac4aa2518e6929a816da509d4280198d65cf6f
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-06-29 13:10:39 +0300

    style: :art: Update to the hero section content

--- src/app/(home)/_components/hero/hero-paragraph.tsx
+++ src/app/(home)/_components/hero/hero-paragraph.tsx
@@ -1,19 +1,33 @@
 const HeroParagraph = () => {
+	let x = "this is me typing and monitoring the cpu usage of github copilot and the cspell extension being working in the same time and catching errors"
 	return (
 		<ul className="max-w-xl text-xl leading-relaxed">
 			<li className="">
-				I focus on <span className="font-bold text-secondary-foreground">performance</span> and <span className="font-bold text-secondary-foreground">accessablity</span> to create the best user experience.
+				I focus on <span className="font-bold text-secondary-foreground">performance</span> and <span className="font-bold text-secondary-foreground">accessibility</span> to create the best user experience.
 			</li>
-			<li className="">
-				<span className="block p-2"></span>I am a pro <span className="font-bold text-secondary">Full-Stack type safety</span>
+			<li className="pt-4">
+				Worked across all layers <br />
+				of building <span className="font-bold text-secondary">scalable full stack applications,</span> from <span className="font-bold text-secondary">front-end</span> to <span className="font-bold text-secondary">back-end</span> and <span className="font-bold text-secondary">deployment.</span>
+			</li>
+			<li className="pt-4 max-sm:hidden">
+				Experienced in the complete software development <br />
+				life cycle, including{" "}
+				<span className="font-bold text-secondary">
+					Development, CI/CD, <br />
+					Testing, Documentation, Monitoring
+				</span>
+				<br />
+				and <span className="font-bold text-secondary">Analytics Collection.</span>
+			</li>
+			{/* <li className="">
+				I am a pro <span className="font-bold text-secondary">Full-Stack type safety</span>
 				{", "}
 				<br />
 				and I always start my projects with TypeScript and <span className="font-bold text-secondary">tRPC</span> whenever possible.
-			</li>
-			<li className="max-sm:hidden">
-				<span className="block p-2 "></span>
-				Finally, I have the ability to learn and be productive with new technologis in a very short time, and I&apos;m allways <span className="font-bold underline decoration-secondary underline-offset-4">open for learning whatever tool</span> needed for the job.
-			</li>
+			</li> */}
+			{/* <li className="pt-4 max-sm:hidden">
+				Finally, I have the ability to learn and be productive with new technologies in a very short time, and I&apos;m always <span className="font-bold underline decoration-secondary underline-offset-4">open for learning whatever tool</span> needed for the job.
+			</li> */}
 		</ul>
 	)
 }
--- src/app/(home)/_components/hero/hero.tsx
+++ src/app/(home)/_components/hero/hero.tsx
@@ -7,11 +7,11 @@ const Hero = () => {
 	return (
 		<div className="relative min-h-[85vh] sm:pt-8 ">
 			<div className="absolute inset-0 w-full  overflow-hidden max-2xl:hidden ">
-				<Image fill className="object-cover" alt="Mountain Image" src="/hero/hero.png" />
+				<Image fill priority={true} className="object-cover" alt="Mountain Image" src="/hero/hero.png" />
 			</div>
 			<div className="  mx-auto max-w-7xl p-4 text-left sm:pl-12 ">
 				<h1 className="relative text-4xl font-bold leading-tight max-sm:text-3xl">
-					I&apos;m a <span className="gradient-text text-5xl font-bold max-sm:block max-sm:text-4xl ">Full-Stack Web Developer</span> <br className="max-sm:hidden" />
+					I&apos;m a <span className="gradient-text text-5xl font-bold max-md:text-4xl max-sm:block ">Full-Stack Web Developer</span> <br className="max-sm:hidden" />
 					with more than two years of experience.
 				</h1>
 				<div className="relative w-fit pb-28 pr-16 max-sm:px-4 max-sm:pt-4 sm:ml-16 sm:pt-8 ">
@@ -21,7 +21,7 @@ const Hero = () => {
 					</div>
 				</div>
 
-				<div className="absolute bottom-0 right-0 flex gap-4 max-sm:hidden">
+				<div className="absolute bottom-0 right-0 flex gap-4 max-md:hidden">
 					<TechnologiesMarquee />
 				</div>
 			</div>
