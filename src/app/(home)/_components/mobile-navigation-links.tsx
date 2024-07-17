98d15efa670b0ce908e839011cfb5565bb32f8ba
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-07-17 11:47:10 +0300

    refactor: :fire: Display the navigation links from inside an array of links instead of hard coding them

--- src/app/(home)/_components/mobile-navigation-links.tsx
+++ src/app/(home)/_components/mobile-navigation-links.tsx
@@ -1,6 +1,8 @@
 import { SheetClose } from "@/components/ui/sheet"
 import { cn } from "@/lib/utils"
 import Link from "next/link"
+import { hiddenLinks, navLinks } from "./navigation-links"
+import type { UrlObject } from "url"
 
 interface NavigationLinksProps {
 	className: string
@@ -9,54 +11,20 @@ interface NavigationLinksProps {
 const MobileNavigationLinks = ({ className }: NavigationLinksProps) => {
 	return (
 		<div className={cn(className, "")}>
-			<SheetClose asChild>
-				<Link href="/#hero" className="p-2">
-					<div className="group/link relative font-bold  text-secondary ">
-						Home
-						<span className="absolute bottom-0 left-0 block h-[2px] w-0  bg-secondary-foreground duration-500 group-hover/link:w-full "></span>
-					</div>
-				</Link>
-			</SheetClose>
-			<SheetClose asChild>
-				<Link href="/#work" className="p-2">
-					<div className="group/link relative font-bold  text-secondary ">
-						Work
-						<span className="absolute bottom-0 left-0 block h-[2px] w-0  bg-secondary-foreground duration-500 group-hover/link:w-full "></span>
-					</div>
-				</Link>
-			</SheetClose>
-			<SheetClose asChild>
-				<Link href="/#projects" className="hidden p-2">
-					<div className="group/link relative font-bold  text-secondary ">
-						Projects
-						<span className="absolute bottom-0 left-0 block h-[2px] w-0  bg-secondary-foreground duration-500 group-hover/link:w-full "></span>
-					</div>
-				</Link>
-			</SheetClose>
-			<SheetClose asChild>
-				<Link href="/#blog" className="hidden p-2">
-					<div className="group/link relative font-bold  text-secondary ">
-						Blog
-						<span className="absolute bottom-0 left-0 block h-[2px] w-0  bg-secondary-foreground duration-500 group-hover/link:w-full "></span>
-					</div>
-				</Link>
-			</SheetClose>
-			<SheetClose asChild>
-				<Link href="/#education	" className="p-2">
-					<div className="group/link relative font-bold  text-secondary ">
-						Education
-						<span className="absolute bottom-0 left-0 block h-[2px] w-0  bg-secondary-foreground duration-500 group-hover/link:w-full "></span>
-					</div>
-				</Link>
-			</SheetClose>
-			<SheetClose asChild>
-				<Link href="/#contact-me" className="p-2">
-					<div className="group/link relative font-bold  text-secondary ">
-						Contact Me
-						<span className="absolute bottom-0 left-0 block h-[2px] w-0  bg-secondary-foreground duration-500 group-hover/link:w-full "></span>
-					</div>
-				</Link>
-			</SheetClose>
+			{navLinks.map((navLink, index) => (
+				<SheetClose key={navLink.name} asChild>
+					<Link
+						key={index}
+						href={navLink.href as unknown as UrlObject}
+						className={cn("p-2", hiddenLinks.includes(navLink.name) && "hidden")}
+					>
+						<div className="group/link relative font-bold tracking-widest text-secondary">
+							{navLink.name}
+							<span className="absolute bottom-0 left-0 block h-[2px] w-0 bg-secondary-foreground duration-500 group-hover/link:w-full"></span>
+						</div>
+					</Link>
+				</SheetClose>
+			))}
 		</div>
 	)
 }
--- src/app/(home)/_components/navigation-links.tsx
+++ src/app/(home)/_components/navigation-links.tsx
@@ -1,49 +1,36 @@
 import { cn } from "@/lib/utils"
 import Link from "next/link"
+import type { UrlObject } from "url"
 
 interface NavigationLinksProps {
 	className: string
 }
 
+export let navLinks = [
+	{ name: "Home", href: "/#hero" },
+	{ name: "Work", href: "/#work" },
+	{ name: "Projects", href: "/#projects" },
+	{ name: "Blog", href: "/#blog" },
+	{ name: "Education", href: "/#education" },
+	{ name: "Contact Me", href: "/#contact-me" },
+]
+export let hiddenLinks = ["Projects", "Blog"]
+
 const NavigationLinks = ({ className }: NavigationLinksProps) => {
 	return (
 		<div className={cn(className, "")}>
-			<Link href="/#hero" className="p-2">
-				<div className="group/link relative font-bold  text-secondary ">
-					Home
-					<span className="absolute bottom-0 left-0 block h-[2px] w-0  bg-secondary-foreground duration-500 group-hover/link:w-full "></span>
-				</div>
-			</Link>
-			<Link href="/#work" className="p-2">
-				<div className="group/link relative font-bold  text-secondary ">
-					Work
-					<span className="absolute bottom-0 left-0 block h-[2px] w-0  bg-secondary-foreground duration-500 group-hover/link:w-full "></span>
-				</div>
-			</Link>
-			<Link href="/#projects" className="hidden p-2">
-				<div className="group/link relative font-bold  text-secondary ">
-					Projects
-					<span className="absolute bottom-0 left-0 block h-[2px] w-0  bg-secondary-foreground duration-500 group-hover/link:w-full "></span>
-				</div>
-			</Link>
-			<Link href="/#blog" className="hidden p-2">
-				<div className="group/link relative font-bold  text-secondary ">
-					Blog
-					<span className="absolute bottom-0 left-0 block h-[2px] w-0  bg-secondary-foreground duration-500 group-hover/link:w-full "></span>
-				</div>
-			</Link>
-			<Link href="/#education	" className="p-2">
-				<div className="group/link relative font-bold  text-secondary ">
-					Education
-					<span className="absolute bottom-0 left-0 block h-[2px] w-0  bg-secondary-foreground duration-500 group-hover/link:w-full "></span>
-				</div>
-			</Link>
-			<Link href="/#contact-me" className="p-2">
-				<div className="group/link relative font-bold  text-secondary ">
-					Contact Me
-					<span className="absolute bottom-0 left-0 block h-[2px] w-0  bg-secondary-foreground duration-500 group-hover/link:w-full "></span>
-				</div>
-			</Link>
+			{navLinks.map((navLink, index) => (
+				<Link
+					key={index}
+					href={navLink.href as unknown as UrlObject}
+					className={cn("p-2", hiddenLinks.includes(navLink.name) && "hidden")}
+				>
+					<div className="group/link relative font-bold tracking-widest text-secondary">
+						{navLink.name}
+						<span className="absolute bottom-0 left-0 block h-[2px] w-0 bg-secondary-foreground duration-500 group-hover/link:w-full"></span>
+					</div>
+				</Link>
+			))}
 		</div>
 	)
 }
