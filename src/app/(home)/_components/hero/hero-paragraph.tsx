8e798b7066def039b523ae2fc0566e3b59e6432b
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-07-24 13:59:58 +0300

    docs: :fire: Updates to the hero section

--- src/app/(home)/_components/hero/hero-paragraph.tsx
+++ src/app/(home)/_components/hero/hero-paragraph.tsx
@@ -3,14 +3,16 @@ const HeroParagraph = () => {
 	return (
 		<ul className="max-w-xl text-xl leading-relaxed">
 			<li className="">
+				Worked across <span className="font-bold text-secondary">all layers</span> of modern web development ranging from{" "}
+				<span className="font-bold text-secondary">
+					building user interfaces to <span className="font-bold text-secondary">backend</span>
+					development
+				</span>{" "}
+				and <span className="font-bold text-secondary">creating CI/CD pipelines.</span>
+			</li>
+			<li className="mt-4">
 				I focus on <span className="font-bold text-secondary-foreground">performance</span> and{" "}
 				<span className="font-bold text-secondary-foreground">accessibility</span> to create the best user experience.
-			</li>
-			<li className="pt-4">
-				Worked across all layers <br />
-				of building <span className="font-bold text-secondary">scalable full stack applications,</span> from{" "}
-				<span className="font-bold text-secondary">front-end</span> to <span className="font-bold text-secondary">back-end</span> and{" "}
-				<span className="font-bold text-secondary">deployment.</span>
 			</li>
 			<li className="pt-4 max-sm:hidden">
 				Experienced in the complete software development <br />
@@ -22,15 +24,6 @@ const HeroParagraph = () => {
 				<br />
 				and <span className="font-bold text-secondary">Analytics Collection.</span>
 			</li>
-			{/* <li className="">
-				I am a pro <span className="font-bold text-secondary">Full-Stack type safety</span>
-				{", "}
-				<br />
-				and I always start my projects with TypeScript and <span className="font-bold text-secondary">tRPC</span> whenever possible.
-			</li> */}
-			{/* <li className="pt-4 max-sm:hidden">
-				Finally, I have the ability to learn and be productive with new technologies in a very short time, and I&apos;m always <span className="font-bold underline decoration-secondary underline-offset-4">open for learning whatever tool</span> needed for the job.
-			</li> */}
 		</ul>
 	)
 }
--- src/app/(home)/_components/hero/hero.tsx
+++ src/app/(home)/_components/hero/hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
 				<h1 className="relative text-4xl font-bold leading-tight max-sm:text-3xl">
 					I&apos;m a <span className="gradient-text text-5xl font-bold max-md:text-4xl max-sm:block">Full-Stack Web Developer</span>{" "}
 					<br className="max-sm:hidden" />
-					with more than two years of experience.
+					with more than three years of experience.
 				</h1>
 				<div className="relative w-fit pb-20 pr-16 max-sm:px-4 max-sm:pt-4 sm:ml-16 sm:pt-6">
 					<HeroParagraph />
