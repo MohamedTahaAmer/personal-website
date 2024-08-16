0cdd05a45267d86e860b1cef7ed9cb6ea33b4f68
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-08-16 07:07:12 +0300

    refactor: :fire: Allways fetch the Hero image, it's size is small, and useing JS to display it only on big screens makes it feel sluggish

--- src/app/(home)/_components/hero/bg-image.tsx
+++ src/app/(home)/_components/hero/bg-image.tsx
@@ -1,14 +1,6 @@
-"use client"
-import useScreenWidth from "@/lib/hooks/get-screen-width"
 import Image from "next/image"
-import { useEffect, useState } from "react"
 
 const BgImage = () => {
-	let screenWidth = useScreenWidth()
-	if (!screenWidth || screenWidth < 1536) {
-		return null
-	}
-
 	return (
 		<div className="absolute inset-0 w-full overflow-hidden max-2xl:hidden">
 			<Image fill priority={true} sizes="(min-width: 1536px) 100vw" className="object-cover" alt="Mountain Image" src="/hero/hero.png" />
